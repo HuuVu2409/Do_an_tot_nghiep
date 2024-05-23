8ccd1942bf16922188f83b153b0455df1a8bd506
HuuVu2409 <145926824+HuuVu2409@users.noreply.github.com>
2024-05-23 11:55:56 +0700

    dsf
    dsf

---
+++ fWaitingScreen.cs
@@ -0,0 +1,20 @@
+namespace testapp
+{
+    public partial class fWaitingScreen : Form
+    {
+        public fWaitingScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void button_product_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Button or PictureBox clicked!");
+        }
+
+        private void button_scan_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
